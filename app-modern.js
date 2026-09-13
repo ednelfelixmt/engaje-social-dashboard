14490bf86093b7167fc6e324c212bd3096fc10ab
Ednel Félix de Macedo <ednel.felixmt@gmail.com>
2026-09-13 09:24:32 -0400

    chore: refresh integrator assets

--- app-modern.js
+++ app-modern.js
@@ -3,4 +3,4 @@
 document.write('<script src="/legacy-app.js?v=20260912-legacy"><\/script>');
 document.write('<script src="/dashboard-enhancer.js?v=20260912-1"><\/script>');
 document.write('<script src="/data-integrity.js?v=20260913-3"><\/script>');
-document.write('<script src="/integration-center.js?v=20260913-2"><\/script>');
+document.write('<script src="/integration-center.js?v=20260913-4"><\/script>');
